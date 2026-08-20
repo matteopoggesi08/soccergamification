bd4d8dce9a283c2fea96b5988d4243af33f3cd56
matteopoggesi08 <matteopoggesi@gmail.com>
2026-08-21 00:30:13 +0200

    Delete path directory

--- path/constants/roles.ts
+++
@@ -1,5 +0,0 @@
-export const TEAM_ROLE_LABELS: Record<'allenatore' | 'vice' | 'collaboratore', string> = {
-  allenatore: 'Allenatore',
-  vice: 'Vice allenatore',
-  collaboratore: 'Collaboratore',
-};
